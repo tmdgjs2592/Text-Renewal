844bfc02569a1b1d466d46d5c5d501a0eb92b790
rogkozl23 <143244287+rogkozl23@users.noreply.github.com>
2023-08-26 13:50:57 -0700

    Update TESSERACTtest1.py

--- TESSERACTtest1.py
+++ TESSERACTtest1.py
@@ -4,8 +4,8 @@ import pytesseract
 # Path to the Tesseract executable (if not in system PATH)
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-# Load the PNG image using PIL (Python Imaging Library)
-image = Image.open(r'C:\Users\rkozl\OneDrive\Desktop\triangles and rectangles\TESSERACTtest1image1.png')
+# Load an image into this function
+image = Image.open('Image.png')
 
 # Perform OCR on the image
 extracted_text = pytesseract.image_to_string(image)
